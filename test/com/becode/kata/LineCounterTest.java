ab5ca2efa3c420c0e608f0c2d93d23d94b1ca86c
Niko <cogani@yahoo.es>
2013-04-15 00:02:35 +0200

    Una linea de código devuelve 1 (rojo)

--- test/com/becode/kata/LineCounterTest.java
+++ test/com/becode/kata/LineCounterTest.java
@@ -19,5 +19,12 @@ public class LineCounterTest {
 		final int countExpected = 0;
 		assertEquals("Linea con SOLO caracteres blancos se descarta de la cuenta", countExpected, lineCounter.countCodeLines());
 	}	
+	
+	@Test
+	public void OneCodeLineReturnOne() {
+		final LineCounter lineCounter = new LineCounter("   		  ");
+		final int countExpected = 1;
+		assertEquals("Una linea de código devuelve 1", countExpected, lineCounter.countCodeLines());
+	}	
 
 }
