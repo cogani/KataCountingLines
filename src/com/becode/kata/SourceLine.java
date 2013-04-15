460c5fd6576813a76950172b793d1b23638f8411
Niko <cogani@yahoo.es>
2013-04-15 14:00:32 +0200

    lineIsOnlyblockComentLineAtMultiplesLines -> refactoring

--- src/com/becode/kata/SourceLine.java
+++ src/com/becode/kata/SourceLine.java
@@ -1,40 +1,53 @@
 package com.becode.kata;
 
-public class SourceLine{
+public class SourceLine {
 	private String codeLine;
 	private boolean activatedCommentMode = false;
 
 	public SourceLine(String line) {
 		this.codeLine = line.trim();
 	}
-	
-	public boolean isCodeLine(boolean legacyCommentMode){
-		if (legacyCommentMode){
-			if(codeLine.contentEquals("*/")){			
-				activatedCommentMode = false;
-			}
-			if (codeLine.endsWith("*/")) return false;
-			else return true;
+
+	public boolean isCodeLine(boolean legacyCommentMode) {
+		if (legacyCommentMode) {
+			findClosingBlockComment();
+			return false;
 		}
-			
-		
-		if (!"".equals(codeLine) && !codeLine.startsWith("//")  && !isBlockComment())
+
+		if (!isBlankLine() && !isComentLine()
+				&& !isBlockComment())
 			return true;
-		else return false;
+		else
+			return false;
 	}
-	
-	private boolean isBlockComment()
-	{
-		if (codeLine.startsWith("/*")){
-			if(!codeLine.endsWith("*/"))
-				activatedCommentMode = true;
-			else activatedCommentMode = false;
+
+	private boolean isBlockComment() {
+		if (codeLine.startsWith("/*")) {
+			findClosingBlockComment();
 			return true;
 		}
 		return false;
 	}
 
+	private void findClosingBlockComment() {
+		if (codeLine.contentEquals("*/"))
+			activatedCommentMode = false;
+		else
+			activatedCommentMode = true;
+	}
+	
+	private boolean isBlankLine(){
+		if("".equals(codeLine)) return true;
+		else return false;
+	}
+	
+	private boolean isComentLine(){
+		if(codeLine.startsWith("//")) return true;
+		else return false;
+	}
+
 	public boolean isActivatedCommentMode() {
 		return activatedCommentMode;
 	}
+
 }
